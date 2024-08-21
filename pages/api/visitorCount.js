9d3ecfb0ac8055c6f2f25314fd66188b5c45b489
unvisr-code <unvisr@icloud.com>
2024-08-21 12:13:25 +0900

    대소문자

--- pages/api/visitorCount.js
+++ pages/api/visitorCount.js
@@ -1,9 +1,6 @@
-let visitorCount = 0; // 메모리에 저장된 방문자 수
+let visitorCount = 0;
 
 export default function handler(req, res) {
-    // 요청이 들어올 때마다 방문자 수 증가
-    visitorCount += 1;
-
-    // 현재 방문자 수를 JSON으로 응답
-    res.status(200).json({ count: visitorCount });
+  visitorCount += 1;
+  res.status(200).json({ count: visitorCount });
 }
