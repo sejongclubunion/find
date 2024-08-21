3431469fcd709351427a4b9040edcaec3d1c87ac
unvisr-code <unvisr@icloud.com>
2024-08-21 12:08:57 +0900

    bug

--- pages/api/visitorCount.js
+++ pages/api/visitorCount.js
@@ -1,9 +1,9 @@
-let visitorCount = 0; // 간단히 메모리에 저장된 방문자 수
+let visitorCount = 0; // 메모리에 저장된 방문자 수
 
 export default function handler(req, res) {
-  // 요청이 들어올 때마다 방문자 수 증가
-  visitorCount += 1;
+    // 요청이 들어올 때마다 방문자 수 증가
+    visitorCount += 1;
 
-  // 현재 방문자 수를 JSON으로 응답
-  res.status(200).json({ count: visitorCount });
+    // 현재 방문자 수를 JSON으로 응답
+    res.status(200).json({ count: visitorCount });
 }
